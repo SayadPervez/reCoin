5ad155e7ad1ba857c1e8b8b38a15a814c0e00ecf
sriram2391 <63790838+sriram2391@users.noreply.github.com>
2020-05-25 18:17:39 +0530

    final2
    check da

--- C++ Files/writeNewPwd.cpp
+++ C++ Files/writeNewPwd.cpp
@@ -27,12 +27,16 @@ int index(string s)
 
 string run(char *argv[])
 {
-    fstream infile;
+    ifstream infile;
     string s= argv[1];
+    string t = ",";
+    string l = "\n";
+    string Whole_file = "";
     int q = index(s);
+    int count = 0;
     string s1= slice(s,0,q);
     string s2= slice(s,q+1,s.size());
-    string type1, type2, type3, type4;
+    string type1, type2, type3, type4, type5;
     infile.open("C:\\Users\\srise\\OneDrive\\Documents\\db.csv");
     if(!infile.good())
     {
@@ -43,24 +47,39 @@ string run(char *argv[])
         while(!infile.eof())
         {
             getline(infile,type1,',');
+            Whole_file = Whole_file + type1 + t;
             getline(infile,type2,',');
             if((type2 == s1))
             {
-                infile << s2;
-                infile.close();
-                return "success";
+                type2 = s1;
+                Whole_file = Whole_file + type2 + t;
+                count++;
             }
             else
             {
                 getline(infile,type3,',');
+                Whole_file = Whole_file + type3 + t;
                 getline(infile,type4,',');
+                Whole_file = Whole_file + type4 + t;
                 getline(infile,type4,'\n');
+                Whole_file = Whole_file + type5 + l;
+
             }
         }
 
     }
     infile.close();
-    return "failure";
+    ofstream outfile;
+    outfile.open("C:\\Users\\srise\\OneDrive\\Documents\\db.csv");
+    outfile << Whole_file;
+    if(count == 1)
+    {
+        return "success";
+    }
+    else
+    {
+        return "failure";
+    }
 }
 
 int main(int argc, char *argv[])
